fd388e31530d57bcd6a1679d51b063269c6f6753
Jakub Latusek <jk.latusek@gmail.com>
2026-01-10 12:39:08 +0100

    init project

---
+++ blocks_love/main.lua
@@ -0,0 +1,14 @@
+function love.load()
+	love.graphics.setBackgroundColor(255, 255, 255)
+end
+
+function love.draw()
+	for y = 1, 18 do
+		for x = 1, 10 do
+			love.graphics.setColor(0.87, 0.87, 0.87)
+			local blockSize = 20
+			local blockDrawSize = blockSize - 1
+			love.graphics.rectangle("fill", (x - 1) * blockSize, (y - 1) * blockSize, blockDrawSize, blockDrawSize)
+		end
+	end
+end
